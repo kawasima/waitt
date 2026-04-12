88f3e67609642c7dd87a6ea3fa4f9c8c5c8a42dd
kawasima <kawasima1016@gmail.com>
2026-04-12 16:57:06 +0900

    fix: Update dependencies and remove Lombok from multiple modules

--- waitt-api/src/main/java/net/unit8/waitt/api/configuration/Feature.java
+++ waitt-api/src/main/java/net/unit8/waitt/api/configuration/Feature.java
@@ -3,13 +3,11 @@ package net.unit8.waitt.api.configuration;
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
-import lombok.Data;
 
 /**
  *
  * @author kawasima
  */
-@Data
 public class Feature implements Serializable {
     private static final long serialVersionUID = 1L;
 
@@ -19,4 +17,19 @@ public class Feature implements Serializable {
     private String type;
 
     private Map<String, String> configuration = new HashMap<>();
+
+    public String getGroupId() { return groupId; }
+    public void setGroupId(String groupId) { this.groupId = groupId; }
+
+    public String getArtifactId() { return artifactId; }
+    public void setArtifactId(String artifactId) { this.artifactId = artifactId; }
+
+    public String getVersion() { return version; }
+    public void setVersion(String version) { this.version = version; }
+
+    public String getType() { return type; }
+    public void setType(String type) { this.type = type; }
+
+    public Map<String, String> getConfiguration() { return configuration; }
+    public void setConfiguration(Map<String, String> configuration) { this.configuration = configuration; }
 }
